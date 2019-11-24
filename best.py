a484b6898f8645043270e7f8c5552f8aca2e2dac
Diptanu Sarkar <diptanusarkar@hotmail.com>
2019-11-23 19:43:31 -0500

    Updated Version 3.1.0

---
+++ best.py
@@ -0,0 +1,14 @@
+"""
+The best accuracy for image classification is obtained by nnet
+Diptanu Sarkar, ds9297@rit.edu
+"""
+from nnet import NeuralNetwork
+
+class Best:
+	def train(self, file_name, model_file, epochs):
+		nnet = NeuralNetwork()
+		nnet.train(file_name, model_file, epochs)
+		
+	def test(self, file_name, model_file, output_file = "best_output.txt"):
+		nnet = NeuralNetwork()
+		nnet.test(file_name, model_file, output_file)
